70ca333538d8011bc80fc417b7ee3ef07b888f4f
Numan <ahmet.aral@ucalgary.ca>
2021-04-12 17:36:45 -0600

    Updates

---
+++ static/js/8.7b43fa9e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpseng471-cada"]=this["webpackJsonpseng471-cada"]||[]).push([[8],{739:function(t,c,e){"use strict";e.r(c);var n,s=e(22),a=(e(0),e(19)),i=e(558),h=e(5),p=a.c.div(n||(n=Object(s.a)(["\n\theight: 30px;\n"])));c.default=function(){return Object(h.jsx)(i.a,{children:Object(h.jsx)(p,{})})}}}]);
+//# sourceMappingURL=8.7b43fa9e.chunk.js.map
--- static/js/8.e83c1439.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpseng471-cada"]=this["webpackJsonpseng471-cada"]||[]).push([[8],{739:function(t,c,e){"use strict";e.r(c);var n,s=e(22),a=(e(0),e(19)),i=e(558),h=e(5),p=a.c.div(n||(n=Object(s.a)(["\n\theight: 30px;\n"])));c.default=function(){return Object(h.jsx)(i.a,{children:Object(h.jsx)(p,{})})}}}]);
-//# sourceMappingURL=8.e83c1439.chunk.js.map
